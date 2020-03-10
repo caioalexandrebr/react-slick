8dda80775f22bb11b86fba595dd1a5090e997787
caio.oliveira <caio.oliveira@gympass.com>
2020-03-10 13:00:05 -0300

    Updates

---
+++ precache-manifest.0a44c5529bd013d4dbdde29f01a088ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8c55ded585f2f47ba5fc557ad9ccff",
+    "url": "/react-slick/index.html"
+  },
+  {
+    "revision": "f0469bfe9d527a94da0d",
+    "url": "/react-slick/static/css/main.f2a2d148.chunk.css"
+  },
+  {
+    "revision": "ba1aa5a66e06591950c5",
+    "url": "/react-slick/static/js/2.e390fcbe.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/react-slick/static/js/2.e390fcbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0469bfe9d527a94da0d",
+    "url": "/react-slick/static/js/main.af5910da.chunk.js"
+  },
+  {
+    "revision": "2417070cb9b7aea6d7fb",
+    "url": "/react-slick/static/js/runtime-main.b87bba7f.js"
+  }
+]);
