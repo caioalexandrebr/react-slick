254f9146f920e1551a5cbc894a83701b744bcf30
caioalexandrebr <caioalexandre.br@gmail.com>
2020-03-10 15:28:15 -0300

    feat(App): customizing arrows

--- src/components/SimpleSlider/index.js
+++ src/components/SimpleSlider/index.js
@@ -3,15 +3,46 @@ import Slider from "react-slick";
 
 import Card from '../Card';
 
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, padding: "10px", borderRadius: "500px", display: "block", background: "rebeccapurple" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, padding: "10px", borderRadius: "500px", display: "block", background: "rebeccapurple", zIndex: "1" }}
+      onClick={onClick}
+    />
+  );
+}
+
 export default class SimpleSlider extends Component {
   render() {
     const settings = {
       dots: true,
       infinite: true,
       speed: 500,
-      arrows: false,
-      slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />,
+      responsive: [
+        {
+          breakpoint: 375,
+          settings: {
+            arrows: false,
+          }
+        }
+      ]
     };
     return (
       <div>
